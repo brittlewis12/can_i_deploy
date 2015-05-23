a09318e2d2c7cb1d19f4d1993382256f3c8aafc3
Britt Lewis <brittlewis12@gmail.com>
2015-05-23 16:28:19 -0400

    Add post action to "/" for setting and releasing lock
    * to lock, requires following params:
        * `text: "lock"`
        * `name`: String
    * to release lock, requires following param:
        * `text: "unlock"`

--- web/controllers/lock_controller.ex
+++ web/controllers/lock_controller.ex
@@ -7,4 +7,11 @@ defmodule CanIDeploy.LockController do
   def index(conn, _params) do
     render conn, "index.json", %{data: LockManager.lock_state}
   end
+
+  def lock(conn, %{"text" => "lock", "user_name" => name}) do
+    render conn, "index.json", %{data: LockManager.lock(name)}
+  end
+  def lock(conn, %{"text" => "unlock"}) do
+    render conn, "index.json", %{data: LockManager.release_lock}
+  end
 end
